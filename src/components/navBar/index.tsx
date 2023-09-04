ee38672059920000211fb0331d070f8bdb229379
Sasha Anshukov <buenosdias2@yandex.ru>
2023-09-04 16:13:19 +0300

    fixed by comments to pull request 29

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC } from 'react';
+import React, { FC } from 'react';
 import { useRef, useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
@@ -16,6 +16,8 @@ const NavBar: FC<{ children: React.ReactNode }> = ({
   const tabsRef = useRef<(HTMLElement | null)[]>([]);
   const [tabBackgroundWidth, setTabBackgroundWidth] = useState<number>(0);
   const [tabBackgroundLeft, setTabBackgroundLeft] = useState<number>(0);
+  const [tabBackgroundTop, setTabBackgroundTop] = useState<number>(0);
+  const [tabBackgroundHeight, setTabBackgroundHeight] = useState<number>(0);
 
   const onSelectedTab = (href: string) => {
     router.push(href);
@@ -26,6 +28,8 @@ const NavBar: FC<{ children: React.ReactNode }> = ({
       const currentTab = tabsRef.current.find((tab) => tab?.getAttribute('href') === pathname);
       setTabBackgroundLeft(currentTab?.offsetLeft ?? 0);
       setTabBackgroundWidth(currentTab?.clientWidth ?? 0);
+      setTabBackgroundTop(currentTab?.offsetTop ?? 0);
+      setTabBackgroundHeight(currentTab?.clientHeight ?? 0);
     };
 
     setTabPosition();
@@ -37,12 +41,13 @@ const NavBar: FC<{ children: React.ReactNode }> = ({
       <div className="bg-white pt-[42px] pb-[150px] flex flex-col">
         <ul
           className="
-            relative flex  mb-[100px]
-            ml-[490px] max-2xl:ml-[250px] max-xl:ml-[140px] max-lg:mx-[20px]">
+            relative flex mb-[100px]
+            ml-[490px] max-2xl:ml-[250px] max-xl:ml-auto max-xl:mr-auto
+            max-lg:flex-col max-w-min max-lg:mx-auto max-lg:my-[20px]">
           {navButtonsData.map((button, index) => {
             return (
               <Link
-                className="mr-2"
+                className="mr-2 max-lg: flex justify-center"
                 href={button.href}
                 key={button.id}
                 ref={(el) => (tabsRef.current[index] = el)}
@@ -50,12 +55,27 @@ const NavBar: FC<{ children: React.ReactNode }> = ({
                   onSelectedTab(button.href);
                 }}>
                 <span
-                  className="absolute z-10 bottom-0 top-0 flex overflow-hidden rounded-2xl cursor-pointer transition-all duration-300"
-                  style={{ left: tabBackgroundLeft, width: tabBackgroundWidth }}>
-                  <span className="h-full w-full rounded-2xl bg-gradient-main from-deep-sky-blue from-0% to-rebecca-purple to-100%" />
+                  className="
+                    absolute z-10 bottom-0 top-0
+                    flex overflow-hidden rounded-2xl
+                    cursor-pointer transition-all duration-300
+                    max-lg:flex-col left-0 right-0"
+                  style={{
+                    left: tabBackgroundLeft,
+                    width: tabBackgroundWidth,
+                    top: tabBackgroundTop,
+                    height: tabBackgroundHeight,
+                  }}>
+                  <span
+                    className="
+                    h-full w-full
+                    rounded-2xl
+                    bg-gradient-main from-deep-sky-blue from-0% to-rebecca-purple to-100%
+                  "
+                  />
                 </span>
                 <li
-                  className={`cursor-pointer  ${
+                  className={`cursor-pointer   ${
                     button.active ? 'text-sm-16 text-white duration-300' : ''
                   }`}>
                   <p className="relative z-20 my-[5px] mx-[16px] whitespace-nowrap">
