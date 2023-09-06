c3bd3798622d32ae74c0c28ca2cdb0c414ca99f8
Aleksandr Trofimov <trofimovalexandr88@gmail.com>
2023-09-06 22:00:46 +0200

    responsive articles layout

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,19 +1,22 @@
-import BlogAverage from "@/components/blogAverage/blogAverage";
-import BlogBig from "@/components/blogBig/blogBig";
-import BlogSmall from "@/components/blogSmall/blogSmall";
-import CardArticle from "@/components/cardArticle/CardArticle";
-import { mokData, ImokData } from "@/utils/mokFile";
+import BlogAverage from '@/components/blogAverage/blogAverage';
+import BlogBig from '@/components/blogBig/blogBig';
+import BlogSmall from '@/components/blogSmall/blogSmall';
+import CardArticle from '@/components/cardArticle/CardArticle';
+import { mokData, ImokData } from '@/utils/mokFile';
 
 const Blog = () => {
-    return (
-        <div className=" max-w-[1338px] mx-auto mt-[62px]">
-            <CardArticle mokData={mokData} />
-            {/* <BlogBig />
-            <BlogSmall />
-            <BlogAverage />
-            <BlogSmall /> */}
-        </div>
-    )
-}
+  //   console.log(mokData);
+
+  return (
+    <>
+      <h2 className="my-10">Placeholder for Promo</h2>
+      <div className="flex flex-wrap  max-w-[1900px] mx-auto gap-5">
+        {mokData.map((item, index) => (
+          <CardArticle key={item.title} mokData={item} index={index} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Blog;
--- src/components/cardArticle/CardArticle.tsx
+++ src/components/cardArticle/CardArticle.tsx
@@ -1,83 +1,63 @@
 import Image from 'next/image';
-import { ImokData } from "@/utils/mokFile";
+import { ImokData } from '@/utils/mokFile';
 import AvatarMini from '../avatar-mini/avatarMini';
 
-const CardArticle = ({ mokData }: { mokData: ImokData[] }) => {
-    return (
-        <>
-            {mokData.map((item, index) => (
-                <div key={index}>
-                    {index === 0 ? (
-                        <div className='grid grid-cols-1'>
-                            <div className='flex gap-[50px]'>
-                                <Image
-                                    src={item.imgArticle}
-                                    alt="логотип"
-                                />
-                                <div className='ml-[45px] w-[550px] my-auto'>
-                                    <h2 className='text-[48px]'>{item.title}</h2>
-                                    <div className='mt-[15px]'>
-                                        <AvatarMini img={item.user.imgAvatar} name={item.user.userName} rank={item.user.rank} />
-                                    </div>
-                                    <p className='mt-[29px] max-w-[500px]'>{item.subtitle}</p>
-                                </div>
-                            </div>
-                        </div>
-                    ) : (
-                        <div className='grid grid-rows-1 grid-cols-3 gap-[100px]'>
-                            <div className='flex flex-col gap-[25px] max-w-[380px]'>
-                                <Image
-                                    src={item.imgArticle}
-                                    alt="логотип"
-                                />
-                                <div className='mt-[25px] max-w-[380px]'>
-                                    <h2 className='text-[28px]'>{item.title}</h2>
-                                    <div className='mt-[15px]'>
-                                        <AvatarMini img={item.user.imgAvatar} name={item.user.userName} rank={item.user.rank} />
-                                    </div>
-                                    <p className='mt-[24px] max-w-[380px]'>{item.subtitle}</p>
-                                </div>
-                            </div>
-                        </div>
-                    )
+const CardArticle = ({ mokData, index }: { mokData: ImokData; index: number }) => {
+  const isFirstThree = index % 5 < 3;
 
-                    }
-                </div>
-                // <div className='grid grid-cols-3' key={index}>
-                //     {index === 0 ? (
-                //         <div className='flex gap-[50px]'>
-                //             <Image
-                //                 src={item.imgArticle}
-                //                 alt="логотип"
-                //             />
-                //             <div className='ml-[45px] w-[550px] my-auto'>
-                //                 <h2 className='text-[48px]'>{item.title}</h2>
-                //                 <div className='mt-[15px]'>
-                //                     <AvatarMini img={item.user.imgAvatar} name={item.user.userName} rank={item.user.rank} />
-                //                 </div>
-                //                 <p className='mt-[29px] max-w-[500px]'>{item.subtitle}</p>
-                //             </div>
-                //         </div>
-                //     ) : (
-                //         <div className='flex flex-col gap-[25px] max-w-[400px]'>
-                //             <Image
-                //                 src={item.imgArticle}
-                //                 alt="логотип"
-                //             />
-                //             <div className='mt-[25px] max-w-[380px]'>
-                //                 <h2 className='text-[28px]'>{item.title}</h2>
-                //                 <div className='mt-[15px]'>
-                //                     <AvatarMini img={item.user.imgAvatar} name={item.user.userName} rank={item.user.rank} />
-                //                 </div>
-                //                 <p className='mt-[24px] max-w-[380px]'>{item.subtitle}</p>
-                //             </div>
-                //         </div>
-                //     )}
-                // </div>
-            ))
-            }
-        </>
-    )
-}
+  return (
+    <>
+      <div
+        className={` p-2 mb-8  rounded-lg mx-2 shadow-md w-[100%]  ${
+          isFirstThree ? 'lg:w-[30%]' : 'lg:w-[47%]'
+        }`}>
+        <Image src={mokData.imgArticle} alt="логотип" className="rounded-xl w-full" />
+        <div className="flex flex-col gap-6 ">
+          <h2 className="text-[24px] ">{mokData.title}</h2>
+          <div className="">
+            <AvatarMini
+              img={mokData.user.imgAvatar}
+              name={mokData.user.userName}
+              rank={mokData.user.rank}
+            />
+          </div>
+          <p className="">{mokData.subtitle}</p>
+        </div>
+      </div>
+    </>
+
+    // <div className='grid grid-cols-3' key={index}>
+    //     {index === 0 ? (
+    //         <div className='flex gap-[50px]'>
+    //             <Image
+    //                 src={item.imgArticle}
+    //                 alt="логотип"
+    //             />
+    //             <div className='ml-[45px] w-[550px] my-auto'>
+    //                 <h2 className='text-[48px]'>{item.title}</h2>
+    //                 <div className='mt-[15px]'>
+    //                     <AvatarMini img={item.user.imgAvatar} name={item.user.userName} rank={item.user.rank} />
+    //                 </div>
+    //                 <p className='mt-[29px] max-w-[500px]'>{item.subtitle}</p>
+    //             </div>
+    //         </div>
+    //     ) : (
+    //         <div className='flex flex-col gap-[25px] max-w-[400px]'>
+    //             <Image
+    //                 src={item.imgArticle}
+    //                 alt="логотип"
+    //             />
+    //             <div className='mt-[25px] max-w-[380px]'>
+    //                 <h2 className='text-[28px]'>{item.title}</h2>
+    //                 <div className='mt-[15px]'>
+    //                     <AvatarMini img={item.user.imgAvatar} name={item.user.userName} rank={item.user.rank} />
+    //                 </div>
+    //                 <p className='mt-[24px] max-w-[380px]'>{item.subtitle}</p>
+    //             </div>
+    //         </div>
+    //     )}
+    // </div>
+  );
+};
 
 export default CardArticle;
