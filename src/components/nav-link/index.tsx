386355773b2bb8f8652e4f5f47323e9a0dfa3fc3
Anna Panteleeva <apanteleevadev@gmail.com>
2023-09-17 14:19:59 +0300

    feat: adds active header/footer link acc to rout

--- src/components/nav-link/index.tsx
+++ src/components/nav-link/index.tsx
@@ -1,16 +1,18 @@
+'use client';
 import { ReactNode } from 'react';
 import Link from 'next/link';
 
 type NavLinkProps = {
   href: string;
   children: ReactNode;
+  styles: string;
 };
 
-export const NavLink = ({ href, children }: NavLinkProps) => {
+export const NavLink = ({ href, children, styles }: NavLinkProps) => {
   return (
     <Link
       href={href}
-      className="flex h-full items-center text-base no-underline hover:bg-gradient-main hover:from-deep-sky-blue hover:to-rebecca-purple hover:bg-clip-text hover:text-transparent">
+      className={`flex h-full items-center text-base no-underline hover:bg-gradient-main hover:from-deep-sky-blue hover:to-rebecca-purple hover:bg-clip-text hover:text-transparent ${styles}`}>
       {children}
     </Link>
   );
