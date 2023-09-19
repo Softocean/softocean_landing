ba488e9b1e45b03fce1ffc8b28b33ff2c93de9b4
Anton Lyscov <anton.lyscov@yandex.ru>
2023-09-19 10:34:56 +0300

    перенес data, сделал клик по карточки и новую страницу, поправил горизонтальный скрол

--- src/components/promoArticle/promoArticle.tsx
+++ src/components/promoArticle/promoArticle.tsx
@@ -1,27 +1,30 @@
 import Image from 'next/image';
-import { ImokData } from '@/utils/mokFile';
+import Link from 'next/link';
+import { ImokData } from '@/data/blog';
 import AvatarMini from '../avatar-mini/avatarMini';
 
 const PromoArticle = ({ mokData }: { mokData: ImokData }) => {
     return (
-        <div className='flex gap-[49px] mb-[92px] rounded-lg mx-2 shadow-md w-[100%] p-2'>
-            <Image
-                src={mokData.imgArticle}
-                alt="логотип"
-                className="rounded-xl w-full"
-            />
-            <div className="flex flex-col my-auto">
-                <h2 className="text-[48px] font-semibold">{mokData.title}</h2>
-                <div className="mt-[10px]">
-                    <AvatarMini
-                        img={mokData.user.imgAvatar}
-                        name={mokData.user.userName}
-                        rank={mokData.user.rank}
-                    />
+        <Link href={`/blog/${mokData.id}`}>
+            <div className='flex flex-col mb-[92px] rounded-lg shadow-md w-[100%] p-2 lg:flex-row lg:gap-10'>
+                <Image
+                    src={mokData.imgArticle}
+                    alt="логотип"
+                    className="rounded-xl md:w-full lg:w-1/2 xl:w-full"
+                />
+                <div className="flex flex-col my-auto">
+                    <h2 className="text-[48px] font-semibold">{mokData.title}</h2>
+                    <div className="mt-[10px]">
+                        <AvatarMini
+                            img={mokData.user.imgAvatar}
+                            name={mokData.user.userName}
+                            rank={mokData.user.rank}
+                        />
+                    </div>
+                    <p className="mt-[30px] text-[18px] font-normal">{mokData.subtitle}</p>
                 </div>
-                <p className="mt-[30px] text-[18px] font-normal">{mokData.subtitle}</p>
             </div>
-        </div>
+        </Link>
     )
 }
 
