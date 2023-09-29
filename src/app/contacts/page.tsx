66bc2eb103e264b1365166cecb0fe2cc5a074542
Dmitry Mytnikau <kfifa3@gmail.com>
2023-09-29 21:33:06 +0300

    fix: contacts

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import ContactsData from '@/components/contacts/contactsData';
-import RequisitesInfo from '@/components/contacts/requisitesInfo';
-import MinistryOfCom from '@/components/contacts/ministryOfCom';
 
 export default function Contacts() {
   return (
-    <div
-      className="
-        ml-[245px] max-2xl:ml-[145px] max-xl:ml-[95px] max-lg:ml-[75px]
-        gap-[200px] max-2xl:gap-[150px] max-xl:gap-[100px] max-lg:gap-[50px]
-        mt-[180px] flex w-fit flex-row items-center">
+    <section className="flex w-full flex-col gap-10 py-5 md:flex-row md:py-10">
       <ContactsData />
-      <div
-        className="
-          flex w-fit flex-row
-          gap-[120px] max-2xl:gap-[100px] max-xl:gap-[80px] max-lg:gap-[60px]">
-        <RequisitesInfo />
-        <MinistryOfCom />
-      </div>
-    </div>
+    </section>
   );
 }
