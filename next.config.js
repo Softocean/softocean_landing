6583fc91a990ce4cb9e770de516b00b10832a99b
63phc <pavel.burns@gmail.com>
2023-10-11 16:00:03 +0200

    reliaze

--- next.config.js
+++ next.config.js
@@ -1,20 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ },
-        use: ['@svgr/webpack'],
-      }
-    );
+  output: 'standalone',
+  webpack: (config) => {
+    // Find the existing rule for SVG files
+//    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'));
+//
+//    // Rule for handling SVG as files
+//    const svgFileRule = {
+//      ...fileLoaderRule,
+//      test: /\.svg$/i,
+//      resourceQuery: /url/,
+//    };
+
+    // Rule for handling SVG as React components
+    const svgComponentRule = {
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: { not: /url/ },
+      use: ['@svgr/webpack'],
+    };
+
+    // Push the new rules to the configuration
+    config.module.rules.push(svgComponentRule);
+
     return config;
   },
 };
