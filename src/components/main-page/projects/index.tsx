56c47e6515fa30df5df2dfa2ef2a524495c9b39a
Aleksandr Trofimov <trofimovalexandr88@gmail.com>
2023-10-23 09:32:22 +0200

    fix: исправил ошибку swiper (#62)

--- src/components/main-page/projects/index.tsx
+++ src/components/main-page/projects/index.tsx
@@ -1,22 +1,22 @@
+'use client';
+
 import CustomSwiper from '@/components/main-page/slider';
 import { slides_products, slides_projects } from '@/data/main-page';
 
-export const Products = () => {
-  return (
-    <section>
-      <h2 className="text-h2-mobile md:text-h2 mb-[60px] font-[700] md:mb-[120px]">PRODUCTS</h2>
-      <CustomSwiper slides={slides_products} />
-    </section>
-  );
-};
-
-
 export const Projects = () => {
   return (
-    <section>
-      <h2 className="text-h2-mobile md:text-h2 mb-[60px] font-[700] md:mb-[120px]">PROJECTS</h2>
-      <CustomSwiper slides={slides_projects} />
-    </section>
+    <>
+      <section>
+        <h2 className="mb-[60px] text-h2-mobile font-[700] md:mb-[120px] md:text-h2">PRODUCTS</h2>
+        <CustomSwiper slides={slides_products} />
+      </section>
+
+      <div className="mb-[160px]" />
+
+      <section>
+        <h2 className="mb-[60px] text-h2-mobile font-[700] md:mb-[120px] md:text-h2">PROJECTS</h2>
+        <CustomSwiper slides={slides_projects} />
+      </section>
+    </>
   );
 };
-
